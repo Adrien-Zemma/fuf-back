5b0535c86e77c0d974679d2d9d49bf805631b17f
Adrien-Zemma <adrien.zemma@epitech.eu>
2021-09-15 12:25:29 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 export async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   //app.use(morgan('dev'))
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
