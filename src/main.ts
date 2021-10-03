6034ab517dfeefcb27ee6b9756be94afc0dbd408
Adrien <adrien.zemma@epitech.eu>
2021-10-03 14:46:41 +0200

    feat(heroku)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 export async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   //app.use(morgan('dev'))
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 
